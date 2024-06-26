f831055943f48047aa862c651f58d7943536462c
Prashant Hatekar <prashanthatekar5585@gmail.com>
2024-06-26 10:56:29 +0530

    Ascending Sort code is completed with how to get input of comma seperated and assign

--- src/arrayExamples/SortArray.java
+++ src/arrayExamples/SortArray.java
@@ -11,26 +11,64 @@ public class SortArray {
 		for(int i=0;i<size;i++) {
 			arr[i]=sc.nextInt();
 		}
-		int[] asendingArray=ascendingSort(arr);
-		for(int i=0;i<asendingArray.length;i++) {
-			System.out.print(asendingArray[i]+" ");
+		int[] ascendingArray=ascendingSort(arr);
+		for(int i=0;i<ascendingArray.length;i++) {
+			System.out.print(ascendingArray[i]+" ");
 		}
+		
 		sc.close();
 	}
+	
 	public static int[] ascendingSort(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) {
-            // Find the minimum element in the remaining unsorted array
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            // Swap the found minimum element with the element at index i
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
-        }
-        return arr;
+		for(int i=0;i<arr.length;i++) {
+			int minIndex=i;
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[minIndex]>arr[j]) {
+					minIndex=j;
+				}
+			}
+			int temp=arr[minIndex];
+			arr[minIndex]=arr[i];
+			arr[i]=temp;
+		}
+		return arr;
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	public static int[] ascendingSort(int[] arr) {
+//		for (int i = 0; i < arr.length - 1; i++) {
+//			int minIndex=i;
+//			System.out.println("Start iteration " + (i + 1) + ", initial minIndex: " + minIndex + " (value: " + arr[minIndex] + ")");
+//            for (int j = i + 1; j < arr.length; j++) {
+//                if (arr[minIndex] > arr[j]) {
+//                    System.out.println("Found new minimum at index " + j + " (value: " + arr[j] + "), previous minimum was at index " + minIndex + " (value: " + arr[minIndex] + ")");
+//                    minIndex = j;
+//                }
+//            }
+//            // Swap the found minimum element with the first element
+//            int temp = arr[minIndex];
+//            arr[minIndex] = arr[i];
+//            arr[i] = temp;
+//            System.out.println("Swapped elements at index " + i + " and " + minIndex + ": " + arr[i] + " <--> " + arr[minIndex]);
+//        }            
+//        return arr;
+//	}
+	
 }
